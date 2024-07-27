d8b2fc2a470571ec308af55c1ef29e2c13d69d6b
eshohag <eshohag@outlook.com>
2024-07-27 20:33:31 +0600

    Add project files.

---
+++ NumberFormats/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace NumberFormats
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double actualValue = 1750075123400.4;
+            string format1 = String.Format("{0:##,####,####}", actualValue);
+            Console.WriteLine(format1);
+            var format2 = String.Format("{0:N}", actualValue); // 1,750,075,123,400,000.50
+            Console.WriteLine(format2);
+
+            Console.ReadLine();
+        }
+    }
+}
